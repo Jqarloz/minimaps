4bfa42aee3e3bf9a0227ffe2cbbcffb99f7e9b77
Jqarloz <juancarlos_hs@hotmail.com>
2021-03-08 00:44:57 -0600

    Curso Tailwaid 09 Sizes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,10 +23,51 @@
         </div>
         <h4>Bienvenido a esta nueva plataforma</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sunt ullam assumenda maxime dolorem sit saepe provident beatae consequuntur laboriosam eius voluptas et in aperiam quae, veniam corrupti totam suscipit.</p>
-        <ul>
+        <ul class="p-4">
             <li>Negocios</li>
             <li>Servicios</li>
         </ul>
+        <div class="bg-black w-64 h-screen">
+
+        </div>
+        <div class="container py-4">
+            <div class="bg-blue-300 h-64">
+                <div class="bg-red-300 h-full w-full md:w-3/4 lg:w-1/2">
+
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <table class="table w-full border-separate lg:border-collapse table-fixed">
+                <thead>
+                    <tr>
+                        <th class="w-1/4">Nombre</th>
+                        <th class="w-1/4">Categoria</th>
+                        <th class="w-1/2">Descripción</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>Negocio 1</td>
+                    <td>Miscelanea</td>
+                    <td>Voluptas rem officiis saepe obcaecati magni nam nisi ullam, quisquam ad aut suscipit praesentium, mollitia hic culpa accusantium harum accusamus reiciendis cumque eligendi! Ab nulla modi totam. Quidem, labore voluptatem.</td>
+                </tr>
+                <tr>
+                    <td>Negocio 2</td>
+                    <td>Zapateria</td>
+                    <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi perspiciatis molestias repellat itaque obcaecati aut beatae ipsam dolor illo quasi non ea aspernatur, rerum quos quia numquam, accusantium veniam facilis.</td>
+                </tr>
+                <tr>
+                    <td>Negocio 3</td>
+                    <td>Restaurant</td>
+                    <td>Quod ad adipisci animi commodi ut nostrum ducimus eum, perspiciatis blanditiis quidem et impedit amet sint? Molestiae voluptatem, corrupti dolorem a reiciendis nostrum ipsam, facilis, eius laborum facere error porro!</td>
+                </tr>
+            </table>
+        </div>
+        <div class="container mx-auto pt-5">
+            <div class="w-64 h-64 bg-gray-400 border-8 border-blue-800 hover:border-opacity-50 rounded-full">
+
+            </div>
+        </div>
         <div class="imagen bg-fixed border-8 border-red-500 bg-clip-padding">
 
         </div>
