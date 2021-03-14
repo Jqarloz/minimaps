e55a7f9767f16d80631b87b23866792814658fb4
Jqarloz <juancarlos_hs@hotmail.com>
2021-03-14 04:33:13 -0600

    ajuste a plantilla Jetstream, pantalla home sin auth

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Livewire\PostComponent;
 //Route::get('/', PostComponent::class);
 Route::get('/', function (){
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
